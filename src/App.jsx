c5867d3fa92b6ec77e8922395c0af4d6c7f60ef1
Jose Cruz <resoj.cruz@gmail.com>
2024-08-27 02:58:54 -0400

    Add panel functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,70 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import ContactInfo from './components/ContactInfo'
+import EducationalInfo from './components/EducationInfo'
+import WorkExperience from './components/WorkExperience'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <header>
+    </header>
+    <main>
+      <div className='cv-input-container'>
+        <Panel
+        title="Contact Information"
+        isActive={activeIndex === 0}
+        onShow={() => setActiveIndex(0)}
+        >
+          <ContactInfo></ContactInfo>
+        </Panel>
+        <Panel
+        title="Education"
+        isActive={activeIndex === 1}
+        onShow={() => setActiveIndex(1)}
+        >
+          <EducationalInfo></EducationalInfo>
+        </Panel>
+        <Panel
+        title="Work Experience"
+        isActive={activeIndex === 2}
+        onShow={() => setActiveIndex(2)}
+        >
+          <WorkExperience></WorkExperience>
+        </Panel>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='cv-display-container'>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </main>
+    <footer>
+    </footer>
     </>
   )
+}
+
+function Panel({
+  title,
+  children,
+  isActive,
+  onShow
+}) {
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={onShow}>
+          Show
+        </button>
+      )}
+    </section>
+  );
 }
 
 export default App
